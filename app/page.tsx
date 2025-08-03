5c45e21fc42a09e4a3bb86cc86ac3920b663460e
SUPERMAN999 <genius20205@gmail.com>
2025-08-03 18:19:22 +0300

    edit2

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            2025 Live Site{" "}
+            2025 Live Site{"XXX "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               Amazing Contents
             </code>
